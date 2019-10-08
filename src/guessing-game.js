09dab4d1f68f138e059a77672314395244aaba78
Ambrance <ambrance@mail.ru>
2019-10-08 15:49:41 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.minNumber = 0;
+    this.maxNumber = 0;
+    this.arr = [];
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.minNumber = min;
+    this.maxNumber = max;
+  }
 
-    }
+  guess() {
+    this.arr = Array(this.maxNumber - this.minNumber + 1)
+      .fill()
+      .map((_, idx) => this.minNumber + idx);
+    return (this.middle = Math.round(
+      (this.arr[0] + this.arr[this.arr.length - 1]) / 2,
+    ));
+  }
 
-    guess() {
+  lower() {
+    this.maxNumber = this.middle;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.minNumber = this.middle;
+  }
 }
 
 module.exports = GuessingGame;
